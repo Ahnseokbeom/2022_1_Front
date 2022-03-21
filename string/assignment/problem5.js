45341b459d3ea2a26b1414f799fae40bc8050e3f
Ahnseokbeom <ajm153123@naver.com>
2022-03-22 08:03:10 +0900

    problem5.js recommit

--- string/assignment/problem5.js
+++ string/assignment/problem5.js
@@ -5,29 +5,14 @@
 */ 
 let a = ["c:/temp/guide.hwp", "c:/pj/frontend/chap05/substring1.js", "homework.docx"]
 // 파일 확장자 출력
-let a1 = a[0].match(/[.][a-z]{2,4}/)
-let a2 = a[1].match(/[.][a-z]{2,4}/)
-let a3 = a[2].match(/[.][a-z]{2,4}/)
-console.log(a1[0])
-console.log(a2[0])
-console.log(a3[0])
-console.log()
-
+for(let i = 0;i<a.length;i++){
+    console.log(a[i].match(/[.][a-z,A-Z]{2,4}/)[0])
+}
 // 파일명 출력
-a1 = a[0].match(/[a-z]{5,10}[.][a-z]{2,4}/)
-a2 = a[1].match(/[a-z]{5,10}.[.][a-z]{2,4}/)
-a3 = a[2].match(/[a-z]{5,10}[.][a-z]{2,4}/)
-console.log(a1[0])
-console.log(a2[0])
-console.log(a3[0])
-console.log()
-
+for(i = 0;i<a.length;i++){
+    console.log(a[i].match(/[0-9,a-z,A-Z]+(?=\.)[.][a-z,A-Z]{2,4}/)[0])
+}
 // 파일 이름 부분만 출력
-a1 = a[0].match(/[a-z]{5,10}/)
-a2 = a[1].match(/[a-z]{9,10}./)
-a3 = a[2].match(/[a-z]{5,10}/)
-console.log(a1[0])
-console.log(a2[0])
-console.log(a3[0])
-
-
+for(i = 0;i<a.length;i++){
+    console.log(a[i].match(/[0-9,a-z,A-Z]+(?=\.)/)[0])
+}
