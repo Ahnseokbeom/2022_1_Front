539b5e55972943607645fc35c5b86a258a20bb4d
Ahnseokbeom <ajm153123@naver.com>
2022-03-30 17:44:25 +0900

    Front_week_5_2 commit

--- array2/assignment/personhw1.js
+++ array2/assignment/personhw1.js
@@ -5,11 +5,3 @@ let persons = [
   { name: "전우치", age: 19 }
 ];
 console.log(persons.sort((p1,p2) => p2.age-p1.age));
-
-//위 persons 배열에서 age 최대값 사람을 찾아서 출력하는 코드를 구현하라.
-// reduce 메소드를 사용하여 구현하라.
-
-let max = persons.reduce((a,b) => a.age>b.age? a:b);
-console.log(max);
-console.log(max.age);
-console.log(max.name);
