49115e929283ff78fd9e80be21c54eeacc1677e4
Ahnseokbeom <ajm153123@naver.com>
2022-04-05 18:27:33 +0900

    organize3 commit

---
+++ practice/organize3.js
@@ -0,0 +1,12 @@
+// person1
+let person = {
+    name: "홍길동",
+    age: 16
+};
+console.log(person); // {name : '홍길동', age : 16}
+console.log(person.name); // 홍길동
+console.log(person.age); // 16
+person1 = {};
+person1.name = "홍길동";
+person1.age = 16;
+console.log(person1) // {name : '홍길동', age : 16}
