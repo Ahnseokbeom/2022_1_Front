a71b65bcf439f2e89bf59b1c2342086e6a15917a
Ahnseokbeom <ajm153123@naver.com>
2022-04-15 21:12:47 +0900

    exam5 commit

--- B201736023/exam5.js
+++ B201736023/exam5.js
@@ -1,7 +1,10 @@
 function add(a, ...values) {
-    let a1 = [a,...values];
-    return a1;
+    let a1 = [...values];
+    return a.concat(a1);
+
 }
 let a = [0, 1];
 add(a, 2, 3, 4, 5);
+let num = [2,3,4,5];
 console.log(a);
+console.log(a.concat(...num));
