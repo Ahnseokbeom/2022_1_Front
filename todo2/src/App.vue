acede84c2573cd58c3d47fbe969f62ce586efde1
Ahnseokbeom <ajm153123@naver.com>
2022-06-04 20:00:55 +0900

    Week13 Practice commit

---
+++ todo2/src/App.vue
@@ -0,0 +1,12 @@
+<template>
+    <div id="app">
+        <TodoList1/>
+    </div>
+</template>
+<script>
+    import TodoList1 from './TodoList1.vue'
+    export default {name: 'App', components: {
+            TodoList1
+        }}
+</script>
+<style scoped="scoped"></style>
