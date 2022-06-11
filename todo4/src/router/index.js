13e663d861c445694e818b70235608c8da90e8ab
Ahnseokbeom <ajm153123@naver.com>
2022-06-11 20:31:13 +0900

    FinalExam Test

---
+++ todo4/src/router/index.js
@@ -0,0 +1,28 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import HomeView from '../views/HomeView.vue'
+import TodoListView from '../views/TodoListView.vue'
+import TodoView from '../views/TodoView.vue'
+
+const routes = [
+  {
+    path: '/',
+    name: 'home',
+    component: HomeView
+  },
+  {
+    path: '/todolist',
+    name: 'todolist',
+    component: TodoListView
+}, {
+    path: '/todo',
+    name: 'todo',
+    component: TodoView
+}
+]
+
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes
+})
+
+export default router
